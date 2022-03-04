28013264904ef58f9ae364f97749588b79617497
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-03-05 01:32:29 +0530

    added bug fixes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import path from 'path';
 import { LeetcodeApi } from "./src/controllers/api.controller.js";
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("Server Running 🔥🔥🔥🔥")
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+    // res.send("Server Running 🔥🔥🔥🔥")
+})
+app.get("/contact",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/contact.html'));
+})
+app.get("/docs",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/documentation.html'));
 })
 
 const api = new LeetcodeApi();
